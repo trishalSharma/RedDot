3283408cfda10cf31182639507e2540f23e62661
trishalSharma <trishalsharma01@gmail.com>
2026-05-03 13:35:16 +0530

    fix: dot route error 404

--- api/dot/[id].js
+++ api/dot/[id].js
@@ -1,41 +1,32 @@
-export default async function handler(req, res) {
+export default function handler(req, res) {
   const { id } = req.query;
 
-  const APP_URL = 'https://sol-mars.vercel.app';
-
+  const APP_URL = `https://${req.headers.host}`;
   const ogImageUrl = `${APP_URL}/api/og?id=${id}`;
 
-  res.setHeader('Content-Type', 'text/html');
+  res.setHeader("Content-Type", "text/html");
 
-  res.send(`
+  res.status(200).send(`
     <!DOCTYPE html>
     <html>
       <head>
         <meta charset="utf-8" />
-
         <title>SolMars 🚀</title>
 
-        <!-- OG TAGS -->
         <meta property="og:title" content="I planted on Mars 🚀" />
         <meta property="og:description" content="Join me in terraforming Mars" />
         <meta property="og:image" content="${ogImageUrl}" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="${APP_URL}/dot/${id}" />
 
-        <!-- TWITTER -->
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="I planted on Mars 🚀" />
         <meta name="twitter:description" content="Join me in terraforming Mars" />
         <meta name="twitter:image" content="${ogImageUrl}" />
 
-        <!-- Redirect user -->
-        <script>
-          window.location.href = "${APP_URL}?dot=${id}";
-        </script>
+        <meta http-equiv="refresh" content="0; url=/?dot=${id}" />
       </head>
-      <body>
-        Redirecting...
-      </body>
+      <body></body>
     </html>
   `);
 }
